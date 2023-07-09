4946cd85253400374ebb916a19b87b557665fd1d
Lautaro Rocha <lautarovrocha@gmail.com>
2023-07-09 15:36:08 -0300

    getSquare

--- script2.js
+++ script2.js
@@ -1,12 +1,17 @@
 ///escribir una función getSquare()
 //que guarde en una variable el cuadrado de un número que reciba 
 //por parametro
-let num = 5;
-const EXPONENT = 2;
 
-function getSquare(variable){
-    let cuadrado = Math.pow(variable, EXPONENT);
-    return cuadrado;
-}
+//En este caso la función estaba bien, no tiene mucho sentido
+//guardar en una variable el exponente, porque la función 
+//busca siempre el cuadrado, así que podríamos 
+//usar directamente el mismo paramétro.
+//como no realizamos validaciones de tipo,
+//podemos usar esta sintaxis de funcion flehca
+//con return ímplicito para dejarlo en una línea.
 
-console.log(getSquare(num));
+const getSquare = num => num * num
+
+const squareOfSix = getSquare(6)
+
+
