a1c6e3c802b085af7354c9be534b9c9fb89e2f17
Lautaro Rocha <lautarovrocha@gmail.com>
2023-07-09 15:59:11 -0300

    returnLongest

--- script3.js
+++ script3.js
@@ -6,17 +6,30 @@
 //si ambos valores son inválidos, devuelve 'None of the values is a string'
 
 function returnLongest(cadena1, cadena2) {
-    if (typeof(cadena1) !== "string" && typeof(cadena2) !== "string") {
-      console.log("None of the values is a string");
-    } else if (typeof(cadena1) !== "string" || typeof(cadena2) !== "string") {
-      console.log("One of the values is not a string");
-    } else if (cadena1.length === cadena2.length) {
-      console.log("They're the same");
-    } else if (cadena1.length > cadena2.length) {
-      console.log("The first one is longer than the second one!");
-    } else {
-      console.log("The second one is longer than the first one!");
-    }
-  }
+  
+  //con este método voy a recuperar el valor del parámetro que pase
+  const getType = (value) => (typeof value !== "string");
 
-returnLongest("Martina", "Martina");
+  //realizo un switch que se corta cuando la expresión del caso
+  //devuelve true, es más legible y más mantenible que un if else con muchos else if
+  switch (true) {
+    case getType(cadena1) && getType(cadena2):
+      console.log("None of the values is a string");
+      break;
+    case getType(cadena1) || getType(cadena2):
+      console.log("One of the values is not a string");
+      break;
+    case cadena1.length === cadena2.length:
+      console.log("They're the same");
+      break;
+    case cadena1.length > cadena2.length:
+      console.log("The first one is longer than the second one!");
+      break;
+    default:
+      console.log("The second one is longer than the first one!");
+  }
+}
+
+
+
+returnLongest('8', '84')
